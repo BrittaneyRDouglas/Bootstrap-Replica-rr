29b9e27dfcf8814c6bcb6e76cc6a3d3e9b82654a
BrittaneyRDouglas <127153896+BrittaneyRDouglas@users.noreply.github.com>
2024-01-26 13:39:51 -0500

    Update App.js

--- public/App.js
+++ public/App.js
@@ -1,1 +1,29 @@
+// import './App.css';
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello World</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import './App.css';
+import TopBar from './components/TopBar';
+import Background from './components/Background';
+import StoresList from './components/StoreListings';
+
+
+function App() {
+  return (
+    <div className="App">
+      <TopBar />
+      <Background />
+      <StoresList />
+    </div>
+  );
+};
+
+export default App;;
